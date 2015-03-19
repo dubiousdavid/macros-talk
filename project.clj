889e1797b0c54cf77e42ca5599444858c9012972
David Sargeant <dsargeant@roomkey.com>
2015-03-19 15:26:46 -0400

    First pass

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject macros-talk "0.1.0"
+  :description "Talk on macros"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])

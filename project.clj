51e685203923938a59d223a9d9ec6420101b7372
David Sargeant <dsargeant@roomkey.com>
2015-03-19 15:41:53 -0400

    Updated project.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject macros-talk "0.1.0"
   :description "Talk on macros"
-  :url ""
+  :url "https://github.com/dubiousdavid/macros-talk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[lein-gorilla "0.3.4"]])

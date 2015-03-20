21cfc13a4291c10854c0985f19d55f208b348d8c
David Sargeant <dsargeant@roomkey.com>
2015-03-20 15:19:23 -0400

    Instaparse.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/dubiousdavid/macros-talk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [instaparse "1.3.5"]]
   :plugins [[lein-gorilla "0.3.4"]])
